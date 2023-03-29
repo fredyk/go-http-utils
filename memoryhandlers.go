95b83ba69626bb3aea6da1a39e1ef79b16d6f6b2
Jhon Magdalena <jfmv.vt@gmail.com>
2023-03-29 13:50:13 +0200

    UPDATE: handler as ServeHttp()

--- memoryhandlers.go
+++ memoryhandlers.go
@@ -15,19 +15,24 @@ func FiberMemoryHandler() fiber.Handler {
 	}
 }
 
-func HttpMemoryHandler() func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		stats, err := getAllMemoryStats()
-		if err != nil {
-			fmt.Printf("Error reading memory stats from /proc/meminfo: %v", err)
-		}
-		bytes, err := easyjson.Marshal(stats)
-		if err != nil {
-			fmt.Printf("Error marshalling memory stats to json: %v", err)
-		}
-		_, err = w.Write(bytes)
-		if err != nil {
-			fmt.Printf("Error writing memory stats to http response: %v", err)
-		}
+type HttpMemoryHandlerImpl struct {
+}
+
+func (h *HttpMemoryHandlerImpl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	stats, err := getAllMemoryStats()
+	if err != nil {
+		fmt.Printf("Error reading memory stats from /proc/meminfo: %v", err)
 	}
+	bytes, err := easyjson.Marshal(stats)
+	if err != nil {
+		fmt.Printf("Error marshalling memory stats to json: %v", err)
+	}
+	_, err = w.Write(bytes)
+	if err != nil {
+		fmt.Printf("Error writing memory stats to http response: %v", err)
+	}
+}
+
+func HttpMemoryHandler() http.Handler {
+	return &HttpMemoryHandlerImpl{}
 }
