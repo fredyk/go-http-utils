dc69af8d8c252e9b89145dfafea20993c3c772f8
Jhon Magdalena <jfmv.vt@gmail.com>
2023-03-29 14:28:50 +0200

    ADD: headers

--- memoryhandlers.go
+++ memoryhandlers.go
@@ -27,6 +27,12 @@ func (h *HttpMemoryHandlerImpl) ServeHTTP(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		fmt.Printf("Error marshalling memory stats to json: %v", err)
 	}
+	// send headers
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(bytes)))
+	// allow all origins
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// send body
 	_, err = w.Write(bytes)
 	if err != nil {
 		fmt.Printf("Error writing memory stats to http response: %v", err)
