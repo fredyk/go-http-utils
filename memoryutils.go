2cbd32ecd6ed0eca676b2db6caf6cec3d4c7dcd4
Jhon Magdalena <jfmv.vt@gmail.com>
2023-04-04 23:27:28 +0200

    FIX: return err

--- memoryutils.go
+++ memoryutils.go
@@ -140,8 +140,8 @@ func parseProcessList() (out []PsEntry, err error) {
 	// Get sysconf _SC_CLK_TCK
 	// get clock ticks, this will return the same as C.sysconf(C._SC_CLK_TCK)
 	clktck, err := sysconf.Sysconf(sysconf.SC_CLK_TCK)
-	if err == nil {
-		fmt.Printf("SC_CLK_TCK: %v\n", clktck)
+	if err != nil {
+		return out, err
 	}
 	totalClockTicks = float64(clktck)
 
